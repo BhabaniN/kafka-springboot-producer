3d1768d1ce6173868ec903d53cc1bb419d534b53
BhabaniN <mrnayak.bhabanishankar@gmail.com>
2022-04-22 12:53:26 +0530

    initia commit

---
+++ src/main/java/com/java/springboot/kafka/learning/libraryeventkafkaproducer/LibraryEventKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.java.springboot.kafka.learning.libraryeventkafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryEventKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/springboot/kafka/learning/libraryeventkafkaproducer/LibraryEventKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springboot.kafka.learning.libraryeventkafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
